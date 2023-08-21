2464012a683d1d34906aca513230a4dfb2f6bb09
portela <jcbastosportela@gmail.com>
2023-08-21 21:01:48 +0200

    first working version

--- logioptionspp/dllmain.cpp
+++ logioptionspp/dllmain.cpp
@@ -3,45 +3,68 @@
 #include <winnt.h>
 #include <detours.h>
 #include <iostream>
-#include <ntstatus.h>  // Include the header for NTSTATUS values
+#include <ntstatus.h> // Include the header for NTSTATUS values
 #include <iostream>
 #include <fstream>
 #include <string>
 #include <mutex>
 
-class Logger {
-private:
-    Logger() {
-        // Open the log file for writing
-        //logFile.open("log.txt", std::ios::app);
+// don't keep this enabled
+// #define ENABLE_CRAZY_LOGGER
+
+#define ASSERT_GOOD(_r, _m)                             \
+    {                                                   \
+        if (!(_r))                                      \
+        {                                               \
+            std::cerr << "ERROR " << (_m) << std::endl; \
+            logger.log(_m);                             \
+            return (_r);                                \
+        }                                               \
     }
 
-    ~Logger() {
-        // Close the log file
-        if (logFile.is_open()) {
+
+class Logger
+{
+private:
+    Logger() = default;
+
+    ~Logger()
+    {
+        // Close the log file if open
+        if (logFile.is_open())
+        {
             logFile.close();
         }
     }
 
     // Declare private copy constructor and assignment operator to prevent copies
-    Logger(const Logger&) = delete;
-    Logger& operator=(const Logger&) = delete;
+    Logger(const Logger &) = delete;
+    Logger &operator=(const Logger &) = delete;
 
 public:
-    static Logger& getInstance() {
+    static Logger &getInstance()
+    {
         static Logger instance;
         return instance;
     }
 
-    void log(const std::string& message) {
+    void log(const std::string &message)
+    {
+#ifdef ENABLE_CRAZY_LOGGER
         std::lock_guard<std::mutex> lock(mutex); // Ensure thread safety
 
-        logFile.open("log.txt", std::ios::app);
+        if (!logFile.is_open())
+        {
+            // TODO: remove hardcoded path
+            logFile.open("C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\log.txt", std::ios::app);
+        }
 
-        if (logFile.is_open()) {
+        if (logFile.is_open())
+        {
             logFile << message << std::endl;
             logFile.close();
         }
+#endif
     }
 
 private:
@@ -49,45 +72,43 @@ private:
     std::mutex mutex;
 };
 
+// NOTE: this is needed in case we use detours to preload.
+// reason is: "The new process will fail to start if the target DLL does not contain a exported function with ordinal #1."
+// source: https://github.com/microsoft/Detours/wiki/DetourCreateProcessWithDlls
 __declspec(dllexport) void __stdcall MyDetoursInitializationFunction(void)
 {
     // This function serves as the entry point for Detours
     // Perform any initialization or setup required for your hooks
-    std::cerr << "merda" << std::endl;
-    Logger& logger = Logger::getInstance();
-    logger.log("MyDetoursInitializationFunction");
+    Logger::getInstance().log("MyDetoursInitializationFunction");
 }
 
 // Define a typedef for the original function signature
-typedef BOOL(WINAPI* QueryFullProcessImageNameW_t)(
+typedef BOOL(WINAPI *QueryFullProcessImageNameW_t)(
     HANDLE hProcess,
     DWORD dwFlags,
     LPWSTR lpExeName,
-    PDWORD lpdwSize
-    );
+    PDWORD lpdwSize);
 
 // Define a typedef for the original function signature
-typedef BOOL(WINAPI* QueryFullProcessImageNameA_t)(
+typedef BOOL(WINAPI *QueryFullProcessImageNameA_t)(
     HANDLE hProcess,
     DWORD dwFlags,
     LPSTR lpExeName,
-    PDWORD lpdwSize
-    );
+    PDWORD lpdwSize);
 
-QueryFullProcessImageNameW_t funcToDetour = (QueryFullProcessImageNameW_t)(0x00007FFB7F5CA320); //Set it at address to detour in
-QueryFullProcessImageNameA_t funcToDetourA = (QueryFullProcessImageNameA_t)(0x00007FFB7F5CA320); //Set it at address to detour in
+QueryFullProcessImageNameW_t funcToDetour = (QueryFullProcessImageNameW_t)(nullptr);  // Set it at address to detour in
+QueryFullProcessImageNameA_t funcToDetourA = (QueryFullProcessImageNameA_t)(nullptr); // Set it at address to detour in
 
 // Declare the custom function with the same signature
 BOOL WINAPI MyQueryFullProcessImageNameW(
     HANDLE hProcess,
     DWORD dwFlags,
     LPWSTR lpExeName,
-    PDWORD lpdwSize
-)
+    PDWORD lpdwSize)
 {
     BOOL ret = TRUE;
     Logger::getInstance().log("MyQueryFullProcessImageNameW");
-    
+
     // Call the original function using Detours
     /*QueryFullProcessImageNameW_t originalFunction =
         (QueryFullProcessImageNameW_t)DetourFindFunction("KernelBase.dll", "QueryFullProcessImageNameW");
@@ -99,7 +120,7 @@ BOOL WINAPI MyQueryFullProcessImageNameW(
 
     wcsncpy_s(lpExeName, *lpdwSize, L"c:\\bla\\chrome.exe\0", *lpdwSize);
     *lpdwSize = wcslen(lpExeName);
-    
+
     // Handle if the original function cannot be found
     return ret;
 }
@@ -109,8 +130,7 @@ BOOL WINAPI MyQueryFullProcessImageNameA(
     HANDLE hProcess,
     DWORD dwFlags,
     LPSTR lpExeName,
-    PDWORD lpdwSize
-)
+    PDWORD lpdwSize)
 {
     BOOL ret = TRUE;
     Logger::getInstance().log("MyQueryFullProcessImageNameA");
@@ -131,62 +151,77 @@ BOOL WINAPI MyQueryFullProcessImageNameA(
     return ret;
 }
 
-#define ASSERT_GOOD(_r, _m) \
-{\
-if(!(_r)){\
-std::cerr << "ERROR " << (_m) << std::endl;\
-logger.log(_m);\
-return (_r);\
-}\
-}
-
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    Logger& logger = Logger::getInstance();
+    Logger &logger = Logger::getInstance();
 
-    if (DetourIsHelperProcess()) {
+    if (DetourIsHelperProcess())
+    {
         return TRUE;
     }
     if (ul_reason_for_call == DLL_PROCESS_ATTACH)
     {
         logger.log("DLL_PROCESS_ATTACH started");
-        bool ret;
+        /*bool ret;
         ret = DetourRestoreAfterWith();
         ASSERT_GOOD(ret, "DetourRestoreAfterWith");
         ret = DisableThreadLibraryCalls(hModule);
         ASSERT_GOOD(ret, "DisableThreadLibraryCalls");
+        */
         // Initialize Detours
+        auto stt = DetourTransactionBegin();
+        ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourTransactionBegin");
+
+        stt = DetourUpdateThread(GetCurrentThread());
+        ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourUpdateThread");
+
+        // NOTE: the line below will not work as it will get from kernel32.dll instead of kernelbase.dll
+        // funcToDetour = QueryFullProcessImageNameW;
+        // logger.log("Address of QueryFullProcessImageNameW is " + std::to_string((unsigned long int)funcToDetour));
+
+        funcToDetour = (QueryFullProcessImageNameW_t)GetProcAddress(GetModuleHandleA("kernelbase.dll"), "QueryFullProcessImageNameW");
+        logger.log("Address of QueryFullProcessImageNameW is " + std::to_string((unsigned long int)funcToDetour));
+
+        // funcToDetourA = QueryFullProcessImageNameA;
+        // logger.log("Address of QueryFullProcessImageNameA is " + std::to_string((unsigned long int)funcToDetourA));
+
+        stt = DetourAttach(&(PVOID &)funcToDetour, MyQueryFullProcessImageNameW);
+        ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourAttach MyQueryFullProcessImageNameW");
+
+        // stt = DetourAttach(&(PVOID&)funcToDetourA, MyQueryFullProcessImageNameA);
+        // ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourAttach MyQueryFullProcessImageNameA");
+
+        stt = DetourTransactionCommit();
+        ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourTransactionCommit");
+
+        logger.log("DLL_PROCESS_ATTACH is ok");
+    }
+
+    if (ul_reason_for_call == DLL_PROCESS_DETACH)
+    {
+        logger.log("DLL_PROCESS_DETACH started");
+        /*bool ret;
+        ret = DetourRestoreAfterWith();
+        ASSERT_GOOD(ret, "DetourRestoreAfterWith");
+        ret = DisableThreadLibraryCalls(hModule);
+        ASSERT_GOOD(ret, "DisableThreadLibraryCalls");
+        */
+        // Initialize Detours
+        // Uninitialize Detours
         auto stt = DetourTransactionBegin();
         ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourTransactionBegin");
         stt = DetourUpdateThread(GetCurrentThread());
         ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourUpdateThread");
         funcToDetour = QueryFullProcessImageNameW;
-        funcToDetourA = QueryFullProcessImageNameA;
-        logger.log("Address of QueryFullProcessImageNameW is " + std::to_string((unsigned long int)funcToDetour));
-        logger.log("Address of QueryFullProcessImageNameA is " + std::to_string((unsigned long int)funcToDetourA));
-        stt = DetourAttach(&(PVOID&)funcToDetour, MyQueryFullProcessImageNameW);
+        stt = DetourDetach(&(PVOID &)funcToDetour, MyQueryFullProcessImageNameW);
         ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourAttach MyQueryFullProcessImageNameW");
-        stt = DetourAttach(&(PVOID&)funcToDetourA, MyQueryFullProcessImageNameA);
-        ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourAttach MyQueryFullProcessImageNameA");
+        // stt = DetourDetach(&(PVOID&)funcToDetourA, MyQueryFullProcessImageNameA);
+        // ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourAttach MyQueryFullProcessImageNameA");
         stt = DetourTransactionCommit();
         ASSERT_GOOD((stt == STATUS_SUCCESS), "DetourTransactionCommit");
 
-        logger.log("DLL_PROCESS_ATTACH is ok");
-        /*WCHAR name[1000];
-        DWORD size = 1000;
-        QueryFullProcessImageNameW(0, 0, name, &size);*/
-    }
-
-    if (ul_reason_for_call == DLL_PROCESS_DETACH) {
-        // Uninitialize Detours
-        DetourTransactionBegin();
-        DetourUpdateThread(GetCurrentThread());
-        DetourDetach(&(PVOID&)funcToDetour, MyQueryFullProcessImageNameW);
-        DetourDetach(&(PVOID&)funcToDetourA, MyQueryFullProcessImageNameA);
-        DetourTransactionCommit();
+        logger.log("DLL_PROCESS_DETACH is ok");
     }
 
     return TRUE;
 }
-
-
--- logioptionspprun/logioptionspprun.cpp
+++ logioptionspprun/logioptionspprun.cpp
@@ -6,17 +6,25 @@
 #include <windows.h>
 #include <detours.h>
 
-DWORD GetProcessIdByName(const wchar_t* processName) {
+static int inject_with_thread();
+static int preload_and_start();
+
+DWORD GetProcessIdByName(const wchar_t *processName)
+{
     DWORD processId = 0;
     HANDLE hSnapshot = CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
 
-    if (hSnapshot != INVALID_HANDLE_VALUE) {
-        PROCESSENTRY32W processEntry;  // Note the 'W' suffix for the wide-character version
+    if (hSnapshot != INVALID_HANDLE_VALUE)
+    {
+        PROCESSENTRY32W processEntry; // Note the 'W' suffix for the wide-character version
         processEntry.dwSize = sizeof(PROCESSENTRY32W);
 
-        if (Process32FirstW(hSnapshot, &processEntry)) {
-            do {
-                if (wcscmp(processEntry.szExeFile, processName) == 0) {  // Using wcscmp for wide-character strings
+        if (Process32FirstW(hSnapshot, &processEntry))
+        {
+            do
+            {
+                if (wcscmp(processEntry.szExeFile, processName) == 0)
+                { // Using wcscmp for wide-character strings
                     processId = processEntry.th32ProcessID;
                     break;
                 }
@@ -29,8 +37,13 @@ DWORD GetProcessIdByName(const wchar_t* processName) {
     return processId;
 }
 
-
 int main()
+{
+    // return preload_and_start();  // Not yet tested
+    return inject_with_thread();
+}
+
+static int preload_and_start()
 {
     STARTUPINFO si;
     PROCESS_INFORMATION pi;
@@ -38,21 +51,18 @@ int main()
     ZeroMemory(&pi, sizeof(PROCESS_INFORMATION));
     si.cb = sizeof(STARTUPINFO);
 
+    // TODO: remove hardcoded paths
     WCHAR la[] = TEXT("C:\\Program Files\\LogiOptionsPlus\\logioptionsplus_agent.exe");
-    //WCHAR la[] = TEXT("C:\\windows\\notepad.exe");
-    //WCHAR la[] = TEXT("C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\test_detour.exe");
-
-    CHAR dllPath[] = "C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\logioptionspp.dll";  // Replace with your DLL's path
-    //CHAR dllPath[] = "C:\\Program Files\\LogiOptionsPlus\\logioptionspp.dll";  // Replace with your DLL's path
+    CHAR dllPath[] = "C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\logioptionspp.dll"; // Replace with your DLL's path
     LPCSTR paths[] = {dllPath};
-    //const char* dllDetouredPath = "C:\\Windows\\System32\\KernelBase.dll";  // Replace with your DLL's path
+
     BOOL ret = DetourCreateProcessWithDlls(
         NULL,
         la,
         NULL,
-        NULL, 
+        NULL,
         FALSE,
-        CREATE_DEFAULT_ERROR_MODE ,
+        CREATE_DEFAULT_ERROR_MODE,
         NULL,
         NULL,
         &si,
@@ -65,87 +75,52 @@ int main()
     return 0;
 }
 
-int _main() {
-    const wchar_t* targetProcessName = L"logioptionsplus_agent.exe";  // Note the 'L' prefix for wide-character string
+static int inject_with_thread()
+{
+    const wchar_t *targetProcessName = L"logioptionsplus_agent.exe"; // Note the 'L' prefix for wide-character string
     DWORD processId = GetProcessIdByName(targetProcessName);
-    std::cout << "The " << targetProcessName << " process id is " << (int)processId << std::endl;
-    //char dllPath[] = "C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\logioptionspp.dll";  // Replace with your DLL's path
-    char dllPath[] = "C:\\Program Files\\LogiOptionsPlus\\logioptionspp.dll";  // Replace with your DLL's path
+    wchar_t dllPath[] = L"C:\\Users\\jcbas\\Projects\\logioptionspp\\logioptionspp\\x64\\Debug\\logioptionspp.dll"; // Replace with your DLL's path
 
-    /*
-    HANDLE hProcess = OpenProcess(PROCESS_CREATE_THREAD | PROCESS_VM_OPERATION |
-        PROCESS_VM_WRITE, FALSE, processId);
-    LPVOID LoadLibraryAddr = (LPVOID)GetProcAddress(GetModuleHandle(L"kernel32.dll"),
-        "LoadLibraryA");
-    LPVOID LLParam = (LPVOID)VirtualAllocEx(hProcess, NULL, strlen(dllPath),
-        MEM_RESERVE | MEM_COMMIT, PAGE_READWRITE);
-    WriteProcessMemory(hProcess, LLParam, dllPath, strlen(dllPath), NULL);
-    CreateRemoteThread(hProcess, NULL, NULL, (LPTHREAD_START_ROUTINE)LoadLibraryAddr,
-        LLParam, NULL, NULL);
-    CloseHandle(hProcess);
-    */
-    
     // Open a handle to the target process (logioptionsplus_agent.exe)
     HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 
-    if (hProcess != NULL) {
-        LPVOID remoteMemory = VirtualAllocEx(hProcess, NULL, sizeof(dllPath), MEM_COMMIT, PAGE_READWRITE);
-
-        if (remoteMemory != NULL) {
-            auto ret = WriteProcessMemory(hProcess, remoteMemory, dllPath, sizeof(dllPath), NULL);
-            if (!ret)
-            {
-                std::cerr << "Failed WriteProcessMemory" << std::endl;
-                return ret;
-            }
-
-            HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0,
-                (LPTHREAD_START_ROUTINE)LoadLibraryW, remoteMemory, 0, NULL);
-
-            if (hThread != NULL) {
-                WaitForSingleObject(hThread, INFINITE);
-                CloseHandle(hThread);
-            }
-
-            VirtualFreeEx(hProcess, remoteMemory, 0, MEM_RELEASE);
-        }
-
-        CloseHandle(hProcess);
-    }
-    else
+    if (hProcess == NULL)
     {
-        std::cout << "failed" << std::endl;
+        std::cerr << "Failed OpenProcess" << std::endl;
+        return -1;
     }
-    /*
-    HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
-    if (hProcess != NULL) {
-        // Allocate memory for the DLL path in the target process
-        LPVOID remoteMemory = VirtualAllocEx(hProcess, NULL, strlen(dllPath) + 1, MEM_COMMIT, PAGE_READWRITE);
 
-        if (remoteMemory != NULL) {
-            // Write the DLL path into the allocated memory
-            WriteProcessMemory(hProcess, remoteMemory, dllPath, strlen(dllPath) + 1, NULL);
+    LPVOID remoteMemory = VirtualAllocEx(hProcess, NULL, wcslen(dllPath), MEM_COMMIT, PAGE_READWRITE);
+    if (remoteMemory == NULL)
+    {
+        std::cerr << "Failed VirtualAllocEx" << std::endl;
+        return -2;
+    }
 
-            // Get the address of LoadLibraryA function from kernel32.dll
-            //LPTHREAD_START_ROUTINE pLoadLibrary = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
-            LPTHREAD_START_ROUTINE pLoadLibrary = (LPTHREAD_START_ROUTINE)GetProcAddress(GetModuleHandleA("kernel32.dll"), "LoadLibraryA");
+    if (!WriteProcessMemory(hProcess, remoteMemory, dllPath, sizeof(dllPath), NULL))
+    {
+        std::cerr << "Failed WriteProcessMemory" << std::endl;
+        return -3;
+    }
 
-            // Create a remote thread in the target process to load the DLL
-            HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, pLoadLibrary, remoteMemory, 0, NULL);
+    HANDLE hThread = CreateRemoteThread(hProcess, NULL, 0, (LPTHREAD_START_ROUTINE)LoadLibraryW, remoteMemory, 0, NULL);
+    if (hThread != NULL)
+    {
+        WaitForSingleObject(hThread, INFINITE);
+        CloseHandle(hThread);
+    }
 
-            if (hThread != NULL) {
-                // Wait for the remote thread to finish
-                WaitForSingleObject(hThread, INFINITE);
-                CloseHandle(hThread);
-            }
+    if (!VirtualFreeEx(hProcess, remoteMemory, 0, MEM_RELEASE))
+    {
+        std::cerr << "Failed VirtualFreeEx" << std::endl;
+        return -4;
+    }
 
-            // Free the allocated memory in the target process
-            VirtualFreeEx(hProcess, remoteMemory, 0, MEM_RELEASE);
-        }
-
-        // Close the handle to the target process
-        CloseHandle(hProcess);
-    }*/
+    if (!CloseHandle(hProcess))
+    {
+        std::cerr << "Failed CloseHandle" << std::endl;
+        return -4;
+    }
 
     return 0;
 }
